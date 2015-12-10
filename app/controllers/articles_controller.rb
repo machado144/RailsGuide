63f57e47618164f4b73d029039cc7a640e13f5b2
machado144 <lucas.dpmachado@gmail.com>
2015-12-10 15:29:39 -0200

    Study Part 1

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,8 +13,12 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.create(article_params)
-    redirect_to @article
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
 
   private
